8baa57c04c487f42a1e2c02e09a5e1d0208e547d
Mayur Panchal <mayur.panchal@shoptimize.in>
2021-09-11 12:22:08 +0530

    feat: :sparkles: Add image support on receiving the text

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const { Telegraf } = require('telegraf');
+const fs = require('fs');
+const { createCanvas, loadImage } = require('canvas');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 console.log('Bot started', process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('I love you Mishti ❤'));
+bot.help((ctx) => ctx.reply('Send me lyrics!'));
+bot.on('text', (ctx) => {
+	console.log(ctx.message.text);
+
+	const width = 1080;
+	const height = 1080;
+
+	const canvas = createCanvas(width, height);
+	const context = canvas.getContext('2d');
+
+	context.fillStyle = '#000';
+	context.fillRect(0, 0, width, height);
+
+	context.font = 'bold 35pt Menlo';
+	context.textAlign = 'center';
+	context.textBaseline = 'top';
+
+	const text = ctx.message.text;
+
+	const textWidth = context.measureText(text).width;
+	context.fillRect(600 - textWidth / 2 - 10, 170 - 5, textWidth + 20, 120);
+	context.fillStyle = '#fff';
+	context.fillText(text, 600, 170);
+
+	const buffer = canvas.toBuffer('image/png');
+	// fs.writeFileSync('./test.png', buffer);
+	bot.telegram.sendPhoto(ctx.message.chat.id, { source: buffer });
+});
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch();
 
